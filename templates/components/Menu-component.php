615312ff37e49d739d4e56a960d23173b2729ec8
Damian Jamroży <damianjamrozy99@gmail.com>
2024-08-08 14:06:00 +0200

    Theme Build Update

---
+++ templates/components/Menu-component.php
@@ -0,0 +1,9 @@
+<!-- menu.php -->
+<nav>
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="about.php">About</a></li>
+        <li><a href="services.php">Services</a></li>
+        <li><a href="contact.php">Contact</a></li>
+    </ul>
+</nav>
