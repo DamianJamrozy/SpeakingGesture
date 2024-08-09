e3ad5051d19f5d8abc13e77eeae5485bbc11e448
Damian Jamroży <damianjamrozy99@gmail.com>
2024-08-09 15:02:09 +0200

    Update web

--- templates/Default.php
+++ templates/Default.php
@@ -1,15 +1,13 @@
 <!-- index.php -->
-<!DOCTYPE html>
-<html lang="pl">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home Page</title>
-</head>
-<body>
-    <?php include 'componetnts/Menu-componetnts.php'; ?>
+<?php include 'components/header-component.php'; ?>
+<?php include 'components/menu-component.php'; ?>
     
-    <h1>Welcome to the Home Page</h1>
+
+<div class="hero">
+    <h1>Sztuczna inteligencja potrafi mówiæ!</h1>
     <p>This is the content of the home page.</p>
+<div>
+
+
 </body>
 </html>
--- templates/components/Menu-component.php
+++ templates/components/Menu-component.php
@@ -1,9 +1,11 @@
 <!-- menu.php -->
+<body>
+
 <nav>
     <ul>
         <li><a href="index.php">Home</a></li>
-        <li><a href="about.php">About</a></li>
         <li><a href="services.php">Services</a></li>
         <li><a href="contact.php">Contact</a></li>
+        <li style="float:right"><a class="active" href="#about">About</a></li>
     </ul>
 </nav>
