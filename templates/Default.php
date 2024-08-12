2b2c16670436279802f28ef811d77a7cec5cea1e
Damian Jamroży <damianjamrozy99@gmail.com>
2024-08-12 15:20:56 +0200

    Footer UPDATE

--- templates/Default.php
+++ templates/Default.php
@@ -8,11 +8,15 @@
             <span class="button-v1 transparent"> 20 gestów - 2000 próbek - ponad 2h nagrań  </span>
             <h1>Sztuczna inteligencja<br>potrafi mówić!</h1>
             <p class="h1-more">Dołącz do nas i naucz się języka migowego z pomocą<br>nowoczesnego narzędzia wspieranego przez sztuczną inteligencję.</p>
+            <div class="button-margin">
+                <a href="services.php"><span class="button-v2" style="margin-right:1em;">Zaloguj się</span></a>
+                <a href="services.php"><span class="button-v1">Przejdź do aplikacji</span></a>
+            </div>
+            <div class="teaser"></div>
         <div>
     </div>
 
 
 
 </div>
-</body>
-</html>
+<?php include 'components/footer-component.php'; ?>
--- templates/components/Footer-component.php
+++ templates/components/Footer-component.php
@@ -1,4 +1,27 @@
 <footer>
-  <p>Author: Hege Refsnes</p>
-  <p><a href="mailto:hege@example.com">hege@example.com</a></p>
+    <div class="footer-left">
+        <h3>Kontakt</h3>
+        <p>D. Jamroży: <a href="mailto:damianjamrozy99@gmail.com">djamrozy@ur.edu.pl</a></p>
+
+        <h3>Współpraca</h3>
+        <p>Chcesz z nami współpracować?<br>Napisz do nas na jeden z powyższych maili!</p>
+    </div>
+    <div class="footer-right">
+        <h3>Adres</h3>
+        <p>Pigonia 1, pokój nr 201<br>Rzeszów, 35-310</p>
+
+        <h3>Media społecznościowe</h3>
+        <p>Facebook</p>
+        <p>Instagram</p>
+    </div>
+    <div class="footer-bottom">
+        <div class="footer-bottom-left copyright">© 2024 SpeakingGesture by Damian Jamroży. All Rights Reserved.</div>
+        <div class="footer-bottom-right">
+            <h3><img style="width:2em;" src="../img/icons/logo-white.svg">Speaking Gesture</h3>
+        </div>
+    </div>
 </footer>
+
+
+</body>
+</html>
