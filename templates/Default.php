29f6a2599b555fbcf82da497d91225f9ca989942
Damian Jamroży <damianjamrozy99@gmail.com>
2024-08-12 14:25:12 +0200

    Hero edit

--- templates/Default.php
+++ templates/Default.php
@@ -2,11 +2,17 @@
 <?php include 'components/header-component.php'; ?>
 <?php include 'components/menu-component.php'; ?>
 
-<div class="hero">
-    <h1>Sztuczna inteligencja potrafi mówić!</h1>
-    <p>This is the content of the home page.</p>
-<div>
+<div class="content">
+    <div class="container">
+        <div class="hero">
+            <span class="button-v1 transparent"> 20 gestów - 2000 próbek - ponad 2h nagrań  </span>
+            <h1>Sztuczna inteligencja<br>potrafi mówić!</h1>
+            <p class="h1-more">Dołącz do nas i naucz się języka migowego z pomocą<br>nowoczesnego narzędzia wspieranego przez sztuczną inteligencję.</p>
+        <div>
+    </div>
 
 
+
+</div>
 </body>
 </html>
--- templates/components/Menu-component.php
+++ templates/components/Menu-component.php
@@ -4,8 +4,8 @@
 <nav>
     <ul>
         <li><a class="logo-bar" href="index.php"><img class="logo-image" src="../img/icons/logo-white.svg">Speaking Gesture</a></li>
-        <li class="nav-right last"><a href="services.php"><span class="button-nav-v1">Przejdź do aplikacji</span></a></li>
-        <li class="nav-right" ><a href="services.php"><span class="button-nav-v2">Zaloguj się</span></a></li>
+        <li class="nav-right last"><a href="services.php"><span class="button-v1">Przejdź do aplikacji</span></a></li>
+        <li class="nav-right" ><a href="services.php"><span class="button-v2">Zaloguj się</span></a></li>
         <li class="nav-right"><a href="#about">O nas</a></li>
         <li class="nav-right"><a href="contact.php">O projekcie</a></li>
         <li class="nav-right"><a class="active" href="#home">Home</a></li>
