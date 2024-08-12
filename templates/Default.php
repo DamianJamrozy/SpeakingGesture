4a201926585af53925c17a37c4af1efd3e6f0baa
Damian Jamroży <damianjamrozy99@gmail.com>
2024-08-12 08:36:19 +0200

    UTF-8 UPDATE Language

--- templates/Default.php
+++ templates/Default.php
@@ -1,10 +1,9 @@
 <!-- index.php -->
 <?php include 'components/header-component.php'; ?>
 <?php include 'components/menu-component.php'; ?>
-    
 
 <div class="hero">
-    <h1>Sztuczna inteligencja potrafi mówiæ!</h1>
+    <h1>Sztuczna inteligencja potrafi mÃ³wiÄ‡!</h1>
     <p>This is the content of the home page.</p>
 <div>
 
