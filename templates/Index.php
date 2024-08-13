117aab7dcb77b8c087d49f516052f7fd7f808a86
Damian Jamroży <damianjamrozy99@gmail.com>
2024-08-13 20:25:01 +0200

    Index finished alfa

---
+++ templates/Index.php
@@ -0,0 +1,102 @@
+<!-- index.php -->
+<?php include 'components/header-component.php'; ?>
+<?php include 'components/menu-component.php'; ?>
+
+<div class="content">
+    <div class="container">
+        <div class="hero">
+            <span class="button-v1 transparent"> 6 gestów - 1100 próbek - ponad 1h nagrań  </span>
+            <h1>Sztuczna inteligencja<br>potrafi mówić!</h1>
+            <p class="h1-more">Dołącz do nas i naucz się języka migowego z pomocą<br>nowoczesnego narzędzia wspieranego przez sztuczną inteligencję.</p>
+            <div class="button-margin">
+                <a href="services.php"><span class="button-v2" style="margin-right:1em;">Zaloguj się</span></a>
+                <a href="services.php"><span class="button-v1">Przejdź do aplikacji</span></a>
+            </div>
+
+            <div class="teaser"></div>
+        </div>
+        <div>
+            <div class="counter">
+                Zaufało nam już ponad 5 osób, w tym takie organizacje jak:
+                <br>
+                <div class="wrapper">
+		            <div class="item"><img alt="" src="../img/brands/ur.png"></div>
+		            <div class="item"><img alt="" src="../img/brands/ur.png"></div>
+                    <div class="item"><img alt="" src="../img/brands/ur.png"></div>
+                    <div class="item"><img alt="" src="../img/brands/ur.png"></div>
+                    <div class="item"><img alt="" src="../img/brands/ur.png"></div>
+	            </div>
+            </div>
+
+            <div class="glow-circle">
+                <h2>Nasz program to<br> nie tylko zera i<br> jedynki!</h2>
+
+                <p>Aplikacja Speaking Gesture składa się z<br>
+                zaawansowanych algorytmów uczenia maszynowego<br>
+                w oparciu o sieci neuronowe.</p>
+            </div>
+
+            <div class="boxes">
+                <div class="box-half">
+                    <img src="">
+                    <h3> Ucz się języka migowego </h3>
+                    <p>Nasza aplikacja oparta na sztucznej inteligencji pozwala na <br>
+                        szybką i bezproblemową naukę języka migowego. Kurs <br>
+                        podzielony jest na kilka kategorii, które są wykorzystywane w <br>
+                        codziennym życiu osób głuchoniemych. Sprawdź jak wyglądają <br>
+                        wybrane gesty i naśladuj ich sekwencje.
+                    </p>
+                    <p><br><a href=""> Przejdź do nauki języka migowego </a></p>
+                </div>
+                <div class="box-half">
+                     <img src="">
+                    <h3> Tłumacz </h3>
+                    <p>Nasza aplikacja tłumacza bazuje na modelu AI tworzonego przez <br>
+                    specjalistów w dziedzinie IT. Wpisz lub powiedz słowo, a nasz <br>
+                    wirtualny asystent pokaże Ci jak wygląda wybrane słowo. <br>
+                    Zamigaj coś do kamery, a nasz program przetłumaczy język <br>
+                    migowy na język polski.
+                    </p>
+                    <p><br><a href=""> Przejdź do tłumacza języka migowego </a></p>
+                </div>
+            </div>
+            <div class="hidden-box-half-left"></div><div class="hidden-box-half-right"></div>
+             
+
+            <div class="box">
+                <div class="box-left">
+                    <img src="">
+                    <h3> Program od środka </h3>
+                    <p> Zaawansowane algorytmy stworzone przez dwóch przyjaciół i <br>
+                    specjalistów w branży IT: inż. Damiana Jamrożego oraz inż. <br>
+                    Patryka Arendta działają w oparciu o kamerę internetową oraz <br>
+                    technologie weebowe. Algorytmy stale śledzą oraz analizują <br>
+                    obraz z kamery wykrywając podobieństwa gestów, Program <br>
+                    umożliwia również przyjmowanie poleceń głosowych oraz <br>
+                    tekstowych tworząc spójne narzędzie edukacyjno - translatoryczne.
+                    </p>
+                    <p><br><a href=""> Wyświetl pracę dyplomową </a></p>
+                </div>
+                <div class="box-right">
+                 
+                </div>
+                
+            </div>
+            <div class="hidden-box"></div>
+            <div class="box">
+                <center><h2>Rozbudowana analiza statystyczna</h2>
+                <p> Odblokuj możliwości analityczne i sprawdź dokładność wykonywanych gestów! <br>
+                Nasza aplikacja w czasie rzeczywistym weryfikuje precyzję twoich ruchów i klasyfikuje je pod względem poprawności. <br>
+                Sprawdź swoje umiejętności i zamigaj już teraz.<br><br>
+                </p>
+                <a href="services.php"><span class="button-v1">Przejdź do aplikacji</span></a>
+                </center>
+            </div>
+            <div class="hidden-box"></div>
+            
+        </div>
+
+
+
+</div>
+<?php include 'components/footer-component.php'; ?>
--- templates/components/Footer-component.php
+++ templates/components/Footer-component.php
@@ -15,7 +15,7 @@
         <p>Instagram</p>
     </div>
     <div class="footer-bottom">
-        <div class="footer-bottom-left copyright">© 2024 SpeakingGesture by Damian Jamroży. All Rights Reserved.</div>
+        <div class="footer-bottom-left copyright">© 2024 SpeakingGesture by <a href="https://github.com/DamianJamrozy">Damian Jamroży</a>. All Rights Reserved.</div>
         <div class="footer-bottom-right">
             <h3><img style="width:2em;" src="../img/icons/logo-white.svg">Speaking Gesture</h3>
         </div>
