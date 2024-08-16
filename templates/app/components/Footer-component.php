dda3a4185192a8e1f2640adaf917b556f3c6d912
Damian Jamroży <damianjamrozy99@gmail.com>
2024-08-16 18:12:08 +0200

    APP Templates

---
+++ templates/app/components/Footer-component.php
@@ -0,0 +1,27 @@
+<footer>
+    <div class="footer-left">
+        <h3>Kontakt</h3>
+        <p>D. Jamroży: <a href="mailto:damianjamrozy99@gmail.com">djamrozy@ur.edu.pl</a></p>
+
+        <h3>Współpraca</h3>
+        <p>Chcesz z nami współpracować?<br>Napisz do nas na jeden z powyższych maili!</p>
+    </div>
+    <div class="footer-right">
+        <h3>Adres</h3>
+        <p>Pigonia 1, pokój nr 201<br>Rzeszów, 35-310</p>
+
+        <h3>Media społecznościowe</h3>
+        <p>Facebook</p>
+        <p>Instagram</p>
+    </div>
+    <div class="footer-bottom">
+        <div class="footer-bottom-left copyright">© 2024 SpeakingGesture by <a href="https://github.com/DamianJamrozy">Damian Jamroży</a>. All Rights Reserved.</div>
+        <div class="footer-bottom-right">
+            <h3><img style="width:2em;" src="../img/icons/logo-white.svg">Speaking Gesture</h3>
+        </div>
+    </div>
+</footer>
+
+
+</body>
+</html>
---
+++ templates/app/components/Header-component.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="../img/icons/logo-white.svg">
+    <title>Speaking Gesture</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/main.css" title="Domyślny styl" type="text/css">
+</head>
